83d59085713c4446098dc7ac81f8b6b07c156c68
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-11-12 16:09:34 -0700

    Added reset button

--- Part2/index.js
+++ Part2/index.js
@@ -6,3 +6,11 @@ function setCard(){
 
     card.style.color = colorInput.value;
 }
+
+function reset() {
+    let cards = document.getElementsByTagName('section');
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.color = 'grey';
+    }
+}
